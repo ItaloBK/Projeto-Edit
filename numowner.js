c4ccf379318c098bf837a696b162e95b788f5a3b
BK_ <81124659+ItaloBK@users.noreply.github.com>
2021-03-23 20:40:01 -0300

    Update numowner.js

--- numowner.js
+++ numowner.js
@@ -1,10 +1,10 @@
-const ownerNumber = ["557187645787@s.whatsapp.net"]
-const OriginalOwner = '557187645787'
+const ownerNumber = ["553399239345@s.whatsapp.net"]
+const OriginalOwner = '553399239345'
 const vcard = 'BEGIN:VCARD\n' 
             + 'VERSION:3.0\n' 
             + 'FN:Meu criador^~^\n' 
             + 'ORG:Ian;\n' 
-            + 'TEL;type=CELL;type=VOICE;waid=557187645787:+55 71 8764-5787\n' 
+            + 'TEL;type=CELL;type=VOICE;waid=553399239345:+55 33 9923-9345\n' 
             + 'END:VCARD'
 
 exports.vcard = vcard
